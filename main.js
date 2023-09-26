0c7765ea83f4c90a30035b3a200645b4cceddbbb
zboston2 <zachary_boston@brown.edu>
2023-09-26 15:47:09 -0400

    more edits

---
+++ main.js
@@ -0,0 +1,15 @@
+function Redirect(){
+    window.location = "index.html"
+}
+
+function Redirect1() {
+    window.location = "index1.html"
+}
+
+function Redirect2() {
+    window.location = "https://google.com"
+}
+
+function Redirect3() {
+    window.location = "https://google.com"
+}
